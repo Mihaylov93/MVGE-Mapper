1c8ee925c2aab32ceea54733119cab8666eed0b0
Petar Petrov <mihaylov.petrov@gmail.com>
2020-04-20 14:50:15 -0400

    Try moving pointer like xvkbd

--- x11sender.cpp
+++ x11sender.cpp
@@ -16,12 +16,41 @@ X11Sender::~X11Sender()
 
 void X11Sender::keyDown(const unsigned int &iKey)
 {
+    //http://t-sato.in.coocan.jp/xvkbd/events.html
+    Window mRoot, mChild, mWindow;
+    int mRoot_x, mRoot_y, mX, mY;
+    unsigned int mMask;
+    int mRevert_to;
+
+    XGetInputFocus(_display, &mWindow, &mRevert_to);
+
+    XQueryPointer(_display, mWindow,&mRoot, &mChild, &mRoot_x, &mRoot_y, &mX, &mY, &mMask);
+    XWarpPointer(_display, None, mWindow, 0, 0, 0, 0, 1, 1);
+    XFlush(_display);
+
     XTestFakeKeyEvent(_display, iKey, True, 0);
+    XFlush(_display);
+
+    XWarpPointer(_display, None, mRoot, 0, 0, 0, 0, mRoot_x, mRoot_y);
     XFlush(_display);
 }
 
 void X11Sender::keyUp(const unsigned int &iKey)
 {
+    Window mRoot, mChild, mWindow;
+    int mRoot_x, mRoot_y, mX, mY;
+    unsigned int mMask;
+    int mRevert_to;
+
+    XGetInputFocus(_display, &mWindow, &mRevert_to);
+
+    XQueryPointer(_display, mWindow,&mRoot, &mChild, &mRoot_x, &mRoot_y, &mX, &mY, &mMask);
+    XWarpPointer(_display, None, mWindow, 0, 0, 0, 0, 1, 1);
+    XFlush(_display);
+
     XTestFakeKeyEvent(_display, iKey, False, 0);
+    XFlush(_display);
+
+    XWarpPointer(_display, None, mRoot, 0, 0, 0, 0, mRoot_x, mRoot_y);
     XFlush(_display);
 }

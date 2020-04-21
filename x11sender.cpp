4b1646d52fee4a7dfc3b3e43d426f8a5f4424114
Petar Petrov <mihaylov.petrov@gmail.com>
2020-04-20 20:42:08 -0400

    Remove space hardcode

--- x11sender.cpp
+++ x11sender.cpp
@@ -19,14 +19,6 @@ X11Sender::X11Sender()
 
     fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 
-
-    /*
-     * The ioctls below will enable the device that is about to be
-     * created, to pass key events, in this case the space key.
-     */
-    ioctl(fd, UI_SET_EVBIT, EV_KEY);
-    ioctl(fd, UI_SET_KEYBIT, KEY_SPACE);
-
     memset(&usetup, 0, sizeof(usetup));
     usetup.id.bustype = BUS_USB;
     usetup.id.vendor = 0x1234; /* sample vendor */
@@ -62,11 +54,12 @@ void X11Sender::emitEvent(int fd, int type, int code, int val)
 void X11Sender::keyDown(const unsigned int &iKey)
 {
 
-
-
+    const unsigned int mKeyCode = iKey-8;
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
+    ioctl(fd, UI_SET_KEYBIT, mKeyCode);
 
     /* Key press, report the event*/
-    emitEvent(fd, EV_KEY, KEY_SPACE, 1);
+    emitEvent(fd, EV_KEY, mKeyCode, 1);
     emitEvent(fd, EV_SYN, SYN_REPORT, 0);
 
 }
@@ -74,6 +67,7 @@ void X11Sender::keyDown(const unsigned int &iKey)
 void X11Sender::keyUp(const unsigned int &iKey)
 {
        /* Key press, report the event*/
-       emitEvent(fd, EV_KEY, KEY_SPACE, 0);
+       const unsigned int mKeyCode = iKey-8;
+       emitEvent(fd, EV_KEY, mKeyCode, 0);
        emitEvent(fd, EV_SYN, SYN_REPORT, 0);
 }
